5a467071dde9e70f5541fb3994c9e31b88181e59
Giovanni Roberts Silva <giovannirobertssilva@outlook.com>
2023-09-27 16:05:34 -0300

    Signed-off-by: Giovanni Roberts Silva <giovannirobertssilva@outlook.com>

--- MeuSite/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MeuSite.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
